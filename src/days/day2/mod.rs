81440acf6c2ca053f7d18dfd8c4771a38b30fa80
Rhydian Jenkins <rhydz@msn.com>
2022-12-01 18:35:18 +0000

    Days being executed from command line

---
+++ src/days/day2/mod.rs
@@ -0,0 +1,3 @@
+pub fn solve() -> &'static str {
+    return "Day two solution";
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::env;
-
 mod days;
 
-fn execute_day(day: usize) -> String {
-    return days::1::exec;
+use std::env;
+
+fn execute_day(day: usize) -> Result<&'static str, &'static str> {
+    match day {
+        1 => Ok(days::day1::solve()),
+        2 => Ok(days::day2::solve()),
+        _ => Err("Oh no"),
+        // _ => null,
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let day = args[1].parse::<usize>().unwrap();
-    let output = execute_day(day);
+    let output = execute_day(day).unwrap();
 
     println!("Part 1: {}", output);
 }
