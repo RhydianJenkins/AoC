6c0e2bb6a0e4a3fb8bf73b24298d2aa3b838a678
Rhydian Jenkins <rhydz@msn.com>
2022-12-08 18:03:22 +0000

    Day 7 - "progress"

--- src/days/day7/mod.rs
+++ src/days/day7/mod.rs
@@ -2,13 +2,23 @@ use std::fs::read_to_string;
 
 #[derive(Debug)]
 #[allow(dead_code)]
-#[allow(dead_code)]
 struct Node<'a> {
-    name: &'a str,
-    value: usize,
-    children: Vec<&'a Node<'a>>,
-    parent: Box<Option<Node<'a>>>,
+    name: String,
+    value: u32,
+    children: Vec<Node<'a>>,
+    parent: Option<Box<&'a Node<'a>>>,
 }
+
+// impl Node {
+// fn caluclate_value(&self) -> u32 {
+// self.value
+// + self
+// .children
+// .iter()
+// .map(|c| c.caluclate_value())
+// .sum::<u32>()
+// }
+// }
 
 pub fn solve() -> &'static str {
     let input = read_to_string("./src/days/day7/input.txt").unwrap();
@@ -20,40 +30,37 @@ pub fn solve() -> &'static str {
 }
 
 #[allow(unused)]
-fn read_tree<'a>(input: &'a Vec<&'a str>) -> Result<Node<'a>, &str> {
-    let mut root = Node {
-        name: "/",
+fn read_tree<'a>(input: &'a Vec<&'a str>) -> Result<Node, &str> {
+    let root = Node {
+        name: "/".to_string(),
         value: 0,
         children: vec![],
-        parent: Box::new(None),
+        parent: None,
     };
 
-    let mut current_node: &Node = &root;
+    let mut current_node = &root;
 
     for line in input {
         if line.contains("cd") {
             if line.contains("/") {
-                current_node = &mut root;
+                current_node = &root;
                 println!("Going to root");
                 continue;
             }
 
             let change_dir_name = line.chars().last().unwrap().to_string();
-            let found_node = current_node
-                .children
-                .iter()
-                .find(|child| *child.name == change_dir_name);
+            let found_child = find_child(&current_node, &change_dir_name);
 
-            if found_node.is_some() {
-                current_node = *found_node.unwrap();
+            if found_child.is_some() {
+                current_node = found_child.unwrap();
                 continue;
             }
 
-            let new_child_node = create_child_node(&change_dir_name).unwrap();
+            let new_child_node = create_node(change_dir_name.to_string(), current_node);
 
-            // TODO new_child_node is gone, how do we then pass it into the push?
-            // current_node.children.push(&new_child_node);
-            // current_node = &new_child_node;
+            let mut current_node_children = &current_node.children;
+            // current_node_children.push(new_child_node); // TODO this doesn't work
+
             continue;
         }
     }
@@ -62,11 +69,18 @@ fn read_tree<'a>(input: &'a Vec<&'a str>) -> Result<Node<'a>, &str> {
 }
 
 #[allow(dead_code)]
-fn create_child_node<'a>(name: &'a str) -> Result<Node<'a>, &'a str> {
-    Ok(Node {
-        name,
+fn create_node<'a>(name: String, parent: &'a Node) -> Node<'a> {
+    Node {
+        name: name.to_string(),
         value: 0,
-        children: vec![],
-        parent: Box::new(None),
-    })
+        children: Vec::new(),
+        parent: Some(Box::new(parent)),
+    }
+}
+
+fn find_child<'a>(input_node: &'a Node<'a>, search_name: &String) -> Option<&'a Node<'a>> {
+    let mut child_iter = input_node.children.iter();
+    let found_child_node = child_iter.find(|child| *child.name == *search_name);
+
+    found_child_node
 }
