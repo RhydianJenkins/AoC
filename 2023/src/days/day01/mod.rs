3f71f796954a344aea0ca571886793312d9b7de3
Rhydian Jenkins <rhydian.jenkins@basekit.com>
2023-12-01 21:56:30 +0000

    Add test for empty input

--- 2023/src/days/day01/mod.rs
+++ 2023/src/days/day01/mod.rs
@@ -1,11 +1,20 @@
 use std::fs::read_to_string;
 
 pub fn solve() -> Result<&'static str, &'static str> {
-    let input = read_to_string("./input.txt");
+    let input = read_to_string("./src/days/day01/input.txt");
 
     if input.is_err() {
         return Err("Error reading input file");
     }
 
     Ok("Not implemented yet")
+}
+
+pub mod tests {
+    use super::solve;
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(solve(), Ok("Not implemented yet"));
+    }
 }
