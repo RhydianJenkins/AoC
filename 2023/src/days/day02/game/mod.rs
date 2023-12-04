ac95d46085596f743fa2f080f02202de2cfb1bb4
Rhydian Jenkins <rhydian.jenkins@basekit.com>
2023-12-04 22:22:46 +0000

    Day 2 solution (part 1)

--- 2023/src/days/day02/game/mod.rs
+++ 2023/src/days/day02/game/mod.rs
@@ -1,18 +1,93 @@
 #[derive(Debug)]
 pub struct Game {
-    id: u8,
+    pub id: u64,
     max_num_red: u8,
     max_num_green: u8,
     max_num_blue: u8,
 }
 
+fn get_id(line: &str) -> Result<u64, &'static str> {
+    let error_msg = "Could not extract id";
+
+    match line.split(" ").nth(1) {
+        Some(id) => Ok(id.replace(":", "").parse::<u64>().map_err(|_| error_msg)?),
+        None => Err(error_msg),
+    }
+}
+
+fn get_max(line: &str, color: &str) -> u8 {
+    line.split(":")
+        .last()
+        .unwrap()
+        .split(";")
+        .map(|part| {
+            part.split(",")
+                .filter(|part| part.contains(color))
+                .map(|part| {
+                    part.replace(color, "")
+                        .replace(" ", "")
+                        .parse::<u8>()
+                        .unwrap()
+                })
+                .next()
+                .unwrap_or(0)
+        })
+        .max()
+        .unwrap()
+}
+
 impl Game {
-    pub fn new(id: u8, max_num_red: u8, max_num_green: u8, max_num_blue: u8) -> Game {
+    pub fn new(line: &str) -> Game {
+        let id = get_id(line).unwrap();
+        let max_num_red = get_max(line, "red");
+        let max_num_green = get_max(line, "green");
+        let max_num_blue = get_max(line, "blue");
+
         Game {
             id,
             max_num_red,
             max_num_green,
             max_num_blue,
         }
+    }
+
+    #[allow(dead_code)]
+    pub fn is_valid(
+        &self,
+        num_red_allowed: u8,
+        num_green_allowed: u8,
+        num_blue_allowed: u8,
+    ) -> bool {
+        self.max_num_red <= num_red_allowed
+            && self.max_num_green <= num_green_allowed
+            && self.max_num_blue <= num_blue_allowed
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn extracting_id_gracefully_fails() {
+        let input = "Some bs string";
+
+        assert_eq!(get_id(input), Err("Could not extract id"))
+    }
+
+    #[test]
+    fn can_extract_id() {
+        let input = "Game 123: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+
+        assert_eq!(get_id(input).unwrap(), 123);
+    }
+
+    #[test]
+    fn can_extract_max_colours() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+
+        assert_eq!(get_max(input, "red"), 4);
+        assert_eq!(get_max(input, "green"), 2);
+        assert_eq!(get_max(input, "blue"), 6);
     }
 }
--- 2023/src/days/day02/mod.rs
+++ 2023/src/days/day02/mod.rs
@@ -2,38 +2,37 @@ mod game;
 
 use game::Game;
 
-fn get_id(line: &str) -> u8 {
-    let id = line
-        .replace("Game ", "")
-        .split(":")
-        .next()
-        .unwrap()
-        .parse::<u8>()
-        .unwrap();
-
-    println!("ID: {}", id);
-
-    id
-}
-
 fn read_games(inputs: Vec<&str>) -> Result<Vec<Game>, &str> {
-    let games = inputs
-        .iter()
-        .map(|line| {
-            let id = get_id(line);
-            let rest = line.split(";").next().unwrap();
-            let _counts = rest.split(";").next().unwrap();
-
-            Game::new(id, 1, 2, 3)
-        })
-        .collect();
+    let games = inputs.iter().map(|input| Game::new(*input)).collect();
 
     Ok(games)
 }
 
+fn get_valid_games(
+    games: Vec<Game>,
+    allowed_red: u8,
+    allowed_green: u8,
+    allowed_blue: u8,
+) -> Vec<Game> {
+    games
+        .into_iter()
+        .filter(|game| game.is_valid(allowed_red, allowed_green, allowed_blue))
+        .collect()
+}
+
+fn count_valid_games(games: &Vec<Game>) -> u64 {
+    let val = games.iter().fold(0, |acc, game| game.id + acc);
+
+    val
+}
+
 pub fn solve() -> Result<String, String> {
     let _inputs: Vec<&str> = include_str!("./input.txt").lines().collect();
-    Ok("".to_string())
+    let games = read_games(_inputs)?;
+    let valid_games = get_valid_games(games, 12, 13, 14);
+    let game_id_counts = count_valid_games(&valid_games);
+
+    Ok(game_id_counts.to_string())
 }
 
 #[cfg(test)]
@@ -42,7 +41,7 @@ mod tests {
 
     #[test]
     fn reads_games_returns_ok() {
-        let result = read_games(vec![
+        let games = read_games(vec![
             "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
             "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
             "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
@@ -50,7 +49,39 @@ mod tests {
             "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         ]);
 
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap().len(), 5);
+        assert!(games.is_ok());
+        assert_eq!(games.unwrap().len(), 5);
+    }
+
+    #[test]
+    fn can_filter_valid_games() {
+        let games = read_games(vec![
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ]);
+
+        let valid_games = get_valid_games(games.unwrap(), 12, 13, 14);
+
+        assert_eq!(valid_games.len(), 3);
+    }
+
+    #[test]
+    fn it_can_count_the_ids() {
+        let games = read_games(vec![
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ]);
+
+        let valid_games = get_valid_games(games.unwrap(), 12, 13, 14);
+
+        let counted = count_valid_games(&valid_games);
+
+        assert_eq!(counted, 8);
     }
 }
