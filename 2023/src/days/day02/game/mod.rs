f785e3cbe06e32f24d38d8b3296ee102bb62effe
Rhydian Jenkins <rhydian.jenkins@basekit.com>
2023-12-06 15:33:18 +0000

    Day 2 part 2 complete

--- 2023/src/days/day02/game/mod.rs
+++ 2023/src/days/day02/game/mod.rs
@@ -1,9 +1,13 @@
 #[derive(Debug)]
+#[allow(dead_code)]
 pub struct Game {
     pub id: u64,
-    max_num_red: u8,
-    max_num_green: u8,
-    max_num_blue: u8,
+    pub min_num_red: u8,
+    pub min_num_green: u8,
+    pub min_num_blue: u8,
+    pub max_num_red: u8,
+    pub max_num_green: u8,
+    pub max_num_blue: u8,
 }
 
 fn get_id(line: &str) -> Result<u64, &'static str> {
@@ -13,6 +17,40 @@ fn get_id(line: &str) -> Result<u64, &'static str> {
         Some(id) => Ok(id.replace(':', "").parse::<u64>().map_err(|_| error_msg)?),
         None => Err(error_msg),
     }
+}
+
+fn get_min_cubes_possible(line: &str) -> (u8, u8, u8) {
+    let rounds = line.split([':', ';']).skip(1);
+    let mut largest_round = (0, 0, 0);
+    let mut largest_total = 0;
+    let mut num_red = 0;
+    let mut num_green = 0;
+    let mut num_blue = 0;
+
+    for round in rounds {
+        let mut total = 0;
+
+        for colour in round.split(',') {
+            let mut split = colour.split(' ').skip(1);
+            let num = split.next().unwrap().parse::<u8>().unwrap();
+
+            match split.next().unwrap() {
+                "red" => num_red = num,
+                "green" => num_green = num,
+                "blue" => num_blue = num,
+                _ => println!("Unrecognised colour '{}'", colour),
+            }
+
+            total += num;
+        }
+
+        if total > largest_total {
+            largest_round = (num_red, num_green, num_blue);
+            largest_total = total;
+        }
+    }
+
+    return largest_round;
 }
 
 fn get_max(line: &str, color: &str) -> u8 {
@@ -39,12 +77,16 @@ fn get_max(line: &str, color: &str) -> u8 {
 impl Game {
     pub fn new(line: &str) -> Game {
         let id = get_id(line).unwrap();
+        let (min_num_red, min_num_green, min_num_blue) = get_min_cubes_possible(line);
         let max_num_red = get_max(line, "red");
         let max_num_green = get_max(line, "green");
         let max_num_blue = get_max(line, "blue");
 
         Game {
             id,
+            min_num_red,
+            min_num_green,
+            min_num_blue,
             max_num_red,
             max_num_green,
             max_num_blue,
@@ -69,6 +111,7 @@ mod tests {
     use super::*;
 
     #[test]
+    #[ignore]
     fn extracting_id_gracefully_fails() {
         let input = "Some bs string";
 
@@ -76,6 +119,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn can_extract_id() {
         let input = "Game 123: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
 
@@ -83,11 +127,23 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn can_extract_max_colours() {
         let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
 
         assert_eq!(get_max(input, "red"), 4);
         assert_eq!(get_max(input, "green"), 2);
         assert_eq!(get_max(input, "blue"), 6);
+    }
+
+    #[test]
+    #[ignore]
+    fn can_find_smallest_game() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        let (min_red, min_green, min_blue) = get_min_cubes_possible(input);
+
+        assert_eq!(min_red, 1);
+        assert_eq!(min_green, 2);
+        assert_eq!(min_blue, 6);
     }
 }
--- 2023/src/days/day02/mod.rs
+++ 2023/src/days/day02/mod.rs
@@ -26,18 +26,42 @@ fn count_valid_games(games: &Vec<Game>) -> u64 {
     val
 }
 
-pub fn solve() -> Result<String, String> {
-    let _inputs: Vec<&str> = include_str!("./input.txt").lines().collect();
-    let games = read_games(_inputs)?;
-    let valid_games = get_valid_games(games, 12, 13, 14);
-    let game_id_counts = count_valid_games(&valid_games);
+fn count_game_powers(games: &Vec<Game>) -> u64 {
+    let power = games.iter().fold(0, |acc, game| {
+        let power = game.max_num_red as u64 * game.max_num_green as u64 * game.max_num_blue as u64;
 
-    Ok(game_id_counts.to_string())
+        power + acc
+    });
+
+    power
+}
+
+pub fn solve() -> Result<String, String> {
+    let inputs: Vec<&str> = include_str!("./input.txt").lines().collect();
+    let games = read_games(inputs)?;
+    // let valid_games = get_valid_games(&games, 12, 13, 14);
+    // let game_id_counts = count_valid_games(&valid_games);
+    let game_power_totals = count_game_powers(&games);
+
+    Ok(game_power_totals.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn calculates_powers() {
+        let games = read_games(vec![
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ]);
+        let power = count_game_powers(&games.unwrap());
+        assert_eq!(power, 2286);
+    }
 
     #[test]
     fn reads_games_returns_ok() {
